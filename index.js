0e86eb35e5137e100aefaa60e367e0ca4bdee70e
wagnao <wagner.correia@gmail.com>
2021-06-19 19:04:40 -0300

    lab fixed

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
+  add(item){
+    this.items.push(item)
+    this.items.sort(function(a, b) {
+    return a - b;
+    } )
+  }
+  get(pos){
+    if ( pos >= 0 && pos < this.length){
+      return this.items[pos]
+    }else{
+      throw new Error("OutOfBounds")
+  }
 }
+  max(){
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[this.length-1]
+    }
+    // return Math.max.apply(null, this.items);
+  }
+  min(){
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+      }else{
+        return this.items[0]
+      }
+      // return Math.min.apply(null, this.items);
+  }
+  sum(){
+    this.items.reduce(function(acc, valorAtual){
+      return acc + valorAtual
+    },0)
+  }
+  avg(){
+    this.items.reduce(function(acc, valorAtual, index){
+      acc = acc + valorAtual
+      if (index == this.items.length-1){
+        return acc / this.items.length
+      }
+      return acc
+    })
+
+  }
+}
+const list = new SortedList
